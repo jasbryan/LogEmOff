f0d9c39c5854997d4d23663f5f56405204526043
Jason Bryant <jasbryan@microsoft.com>
2019-01-21 10:00:06 -0800

    geneva integration tests

--- LogEmOffUI/Program.cs
+++ LogEmOffUI/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Cloud.InstrumentationFramework;
+
 
 namespace LogEmOffUI
 {
@@ -16,6 +18,48 @@ namespace LogEmOffUI
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            EmitMetrics();
+
+            // IFx initialization is a required step for emitting logs
+            IfxInitializer.Initialize(
+                "cloudAgentTenantIdentity",
+                "cloudAgentRoleIdentity",
+                "cloudAgentRoleInstanceIdentity");
+
+            EmitLogs();
+        }
+
+        static void EmitMetrics()
+        {
+            ErrorContext mdmError = new ErrorContext();
+
+            MeasureMetric1D testMeasure = MeasureMetric1D.Create(
+                "MyMonitoringAccount",
+                "MyMetricNamespace",
+                "MyMetricName",
+                "MyDimensionName",
+                ref mdmError);
+
+            if (testMeasure == null)
+            {
+                Console.WriteLine("Fail to create MeasureMetric, error code is {0:X}, error message is {1}",
+                    mdmError.ErrorCode,
+                    mdmError.ErrorMessage);
+            }
+            else if (!testMeasure.LogValue(101, "MyDimensionValue", ref mdmError))
+            {
+                Console.WriteLine("Fail to log MeasureMetric value, error code is {0:X}, error message is {1}",
+                    mdmError.ErrorCode,
+                    mdmError.ErrorMessage);
+            }
+        }
+
+        static void EmitLogs()
+        {
+            using (Operation operation = new Operation("Some Operation"))
+            {
+                operation.SetResult(OperationResult.Success);
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
